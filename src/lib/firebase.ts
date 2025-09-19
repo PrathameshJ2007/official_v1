dae1a635bbef6a7d5473b20dfcd12d300e8e1e16
Prathamesh Jagtap <pj923839@gmail.com>
2025-09-19 08:22:49 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,9 +2,15 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore }from "firebase/firestore";
 
-// Your web app's Firebase configuration is not available. 
-// Please go to the Firebase console to generate it.
-const firebaseConfig = {};
+// Your web app's Firebase configuration.
+const firebaseConfig = {
+  "projectId": "legaleagletest-31764966-1eb27",
+  "appId": "1:759171583824:web:d1b9d9a76f9398ad7f2655",
+  "apiKey": "AIzaSyCeI8UVTygApx3ApUrgGVXnYwkA_DrwJSM",
+  "authDomain": "legaleagletest-31764966-1eb27.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "759171583824"
+};
 
 // Initialize Firebase
 let app;
